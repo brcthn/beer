e3791a1ef34aafda9ed3cdcd06d7f4d8f2f64e88
Burcu Tahan <brcthn@Burcus-MacBook-Air.local>
2019-07-26 02:58:04 +0200

    alistirma

---
+++ src/main/java/com/brcthn/beer/BeerApplication.java
@@ -0,0 +1,13 @@
+package com.brcthn.beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerApplication.class, args);
+	}
+
+}
